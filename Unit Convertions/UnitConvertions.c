fad4a19ea7212fd378e0229c4a32b78efa3b8956
Trent Patterson <jtrent238@outlook.com>
2019-04-15 12:25:51 -0400

    41520191225

---
+++ Unit Convertions/UnitConvertions.c
@@ -0,0 +1,20 @@
+// Converts Feet to Inches
+double FeetToInches(double feet) {
+	double inches = 0.0;
+	inches = feet * 12;
+	return inches;
+}
+
+// Converts Inches to Feet
+double InchesToFeet(double inches) {
+	double feet = 0.0;
+	feet = inches / 12;
+	return feet;
+}
+
+// Converts Inches to Centimeters
+double InchesToCentimeters(double inches) {
+	double centimeters = 0.0;
+	centimeters = inches * 2.54;
+	return centimeters;
+}
---
+++ Unit Convertions/UnitConvertions.h
@@ -0,0 +1,8 @@
+// Converts Feet to Inches
+double FeetToInches(double feet);
+
+// Converts Inches to Feet
+double InchesToFeet(double inches);
+
+// Converts Inches to Centimeters
+double InchesToCentimeters(double inches) 
